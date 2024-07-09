0a60efbe0b4cc04b532895e5c63a189d990a374e
MuzakiPC <zakizuhdi360@gmail.com>
2024-07-09 10:53:11 +0700

    zaki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="">
-    <header class="">
+<body>
+    <header>
         <div class="text-center py-3">
             <div class="container">
                 <div class="row gx-5 justify-content-center">
@@ -29,8 +29,8 @@
         </div>
         <!-- Image and text -->
         <div class="py-1" style="background-color: #C200BA;"></div>
-        <nav class="bg-secondary-subtle py-2">
-            <div class="row">
+        <nav class="py-2" style="background-image: url({{asset('logos')}}/nap.png);">
+            <div class="d-flex justify-content-center">
                 <div class="nav col-sm-4 justify-content-center align-content-center">
                     <a class="navbar-brand" href="#">
                         <img src="{{asset('logos')}}/logo-sanki.png" width="45" height="45" class="d-inline-block align-top" alt="">
@@ -38,11 +38,11 @@
                 </div>
                 <div class="col-sm-4">
                     <ul class="nav justify-content-center align-middle py-2">
-                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none">PROFIL</a></li>
-                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none">SOP</a></li>
-                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none">KONTAK</a></li>
-                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none">FAQ</a></li>
-                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none">CSIRT</a></li>
+                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none"><b>PROFIL</b></a></li>
+                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none"><b>SOP</b></a></li>
+                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none"><b>KONTAK</b></a></li>
+                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none"><b>FAQ</b></a></li>
+                        <li><a href="#" class="px-2 text-dark" style="text-decoration:none"><b>CSIRT</b></a></li>
                     </ul>
                 </div>
                 <div class="nav col-sm-4 justify-content-center">
@@ -50,50 +50,53 @@
             </div>
         </nav>
     </header>
-    <div class="text-center mt-4 mb-4">
-        <h3><b>SANKI SUPPORT</b></h3>
-    </div>
-    <div class="card-deck container mt-2 mb-2" style="width: 60rem;">
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
+    <section>
+        <div class="text-center mt-4 mb-4">
+            <h3><b>SANKI SUPPORT</b></h3>
         </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
+        <div class="card-deck container mt-2 mb-2">
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
         </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
+        <div class="card-deck container mt-2 mb-2">
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
+            <div class="card">
+                <img class="card-body" src="{{asset('logos')}}/layanan1a.png" alt="Card image cap">
+            </div>
         </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
-        </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
-        </div>
-    </div>
-    <div class="card-deck container mt-2 mb-2" style="width: 60rem;">
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
-        </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
-        </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
-        </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
-        </div>
-        <div class="card">
-            <img class="card-body" src="{{asset('logos')}}/layanan1.png" alt="Card image cap">
-        </div>
-    </div>
-    <footer class="fixed-bottom">
+    </section>
+    <footer class="mt-5" style="background-image: url({{asset('logos')}}/nap.png);">
         <div class="py-1" style="background-color: #C200BA;"></div>
-        <div class="card text-center bg-secondary-subtle">
-            <div class="card-body">
+        <div class="text-center">
+            <div class="py-3">
                 <p class="card-text">Hak Cipta @2024 Dinas Komunikasi dan Informatika Kota Kediri</p>
             </div>
         </div>
+        <!-- <div class="py-1" style="background-color: #C200BA;"></div> -->
     </footer>
 </body>
 
@@ -105,19 +108,9 @@
 <!-- AdminLTE App -->
 <script src="{{asset('lte')}}/dist/js/adminlte.min.js"></script>
 <div class="wrapper">
-    <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
         <img class="animation__shake" src="{{asset('lte')}}/dist/img/sanki_logo.png" alt="Sanki Logo" height="160" width="160">
     </div>
-    <!-- FullCalendar -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
