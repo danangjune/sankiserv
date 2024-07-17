3f3284dba0e5d5cff754f4e43fe26ad864a7d379
MuzakiPC <zakizuhdi360@gmail.com>
2024-07-17 11:27:33 +0700

    zaki-landingpage-tambah

---
+++ resources/views/sop.blade.php
@@ -0,0 +1,78 @@
+@include('frontlayout.header')
+@include('frontlayout.navbar')
+
+<body>
+    <section class="container">
+        <div class="text-center mt-4 mb-4">
+            <h3><b>STANDAR OPERASIONAL PROSEDUR (SOP)</b></h3>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Uraian</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>APTIKA_01 SOP Eskalasi Penanganan Insiden Keamanan SPBE ke BSSN</td>
+                    <td><button type="button" class="btn btn-primary mr-1">Lihat</button><button type="button" class="btn btn-warning">Unduh</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</body>
+
+@include('frontlayout.footer')
