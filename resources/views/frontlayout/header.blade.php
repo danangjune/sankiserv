75fc0f5306d3d6f3d02b230eb7f5317094656fea
MuzakiPC <zakizuhdi360@gmail.com>
2024-07-19 10:22:05 +0700

    zaki-add-navbar-delete-comment

--- resources/views/frontlayout/header.blade.php
+++ resources/views/frontlayout/header.blade.php
@@ -9,7 +9,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Google Font: Source Sans Pro -->
+    <!-- Google Fontz: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{asset('lte')}}/plugins/fontawesome-free/css/all.min.css">
